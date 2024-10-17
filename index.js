659aba1f1918a8b838b16d2bf5014bb00f570791
murillin20070 <00001106020947sp@aluno.educacao.sp.gov.br>
2024-10-17 13:49:27 -0300

    mudancas da aparencia

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const avanca = document.querySelectorAll(".btn-proximo");
 
 avanca.forEach(button => {
  button.addEventListener("click" , function(){
-    
+const atual = querySelector (".ativo")
+const ProxiimoPasso = "passo" + this.getAtribute("data-proximo")
+atual.classlist.remove("ativo");
+document.getElementById(proximopasso).classList.add("ativo");
  })
 })
