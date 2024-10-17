46b904069676622f1b86932e8b510dd7dbe3e09f
murillin20070 <00001106020947sp@aluno.educacao.sp.gov.br>
2024-10-17 13:54:21 -0300

    arrumando erro

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const avanca = document.querySelectorAll(".btn-proximo");
 
 avanca.forEach(button => {
- button.addEventListener("click" , function(){
-const atual = querySelector (".ativo")
-const ProxiimoPasso = "passo" + this.getAtribute("data-proximo")
-atual.classlist.remove("ativo");
-document.getElementById(proximopasso).classList.add("ativo");
+    button.addEventListener("click" , function(){
+        const atual = querySelector (".ativo")
+        const proximoPasso = "passo" + this.getAtribute("data-proximo")
+        atual.classlist.remove("ativo");
+        document.getElementById(proximoPasso).classList.add("ativo");
  })
 })
